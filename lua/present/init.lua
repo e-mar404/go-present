e459cf6afa40d5545539f11515a3f27f7adbe97c
e-mar404 <egmarinn@gmail.com>
2025-08-14 23:14:23 -0500

    use msgpack to actually talk with lua and add some comments on how the flow could work

--- lua/present/init.lua
+++ lua/present/init.lua
@@ -1,21 +1,41 @@
+local uv = vim.uv
+
 local M = {}
 
 M.present = function ()
-  local info = debug.getinfo(1, "S")
-  local current_path = info.source:sub(2)
-  local server_path = current_path:gsub("lua/present/init.lua", "main.go")
+  local rpc_port = 8081
+  local host = "127.0.0.1"
+  local client = uv.new_tcp()
 
-  local cmd = {"go", "run", server_path}
-  local job_id = vim.fn.jobstart(cmd, {
-    on_stdout = function (id, data, event)
-      print(vim.inspect(data), event)
-    end,
-    rpc = true, -- on_stdout gets disabled if this is on
+  -- should first check if the server is running rpc server 
+  -- if it isnt then it should do all the things bellow
+  client:connect(host, rpc_port, function (err)
+    --check if connection was successful 
+  end)
+
+  -- start server
+  -- local info = debug.getinfo(1, "S")
+  -- local current_path = info.source:sub(2)
+  -- local server_path = current_path:gsub("lua/present/init.lua", "main.go")
+  --
+  -- local on_exit = function(obj)
+  --   print(obj.code)
+  --   print(obj.signal)
+  --   print(obj.stdout)
+  --   print(obj.stderr)
+  -- end
+  -- local _obj = vim.system({"go", "run", server_path}, {}, on_exit):wait()
+
+  -- will use defer_fn to periodically check after it gets turned and if it times out then close the entire program and exit with an error
+  vim.defer_fn(function ()
+  end, 5000)
+
+  local chan = vim.fn.sockconnect("tcp", "127.0.0.1:8081", {
+    rpc = true,
   })
-
-  local result = vim.fn.rpcrequest(job_id, "Present.NextSlide")
-
-  print("result from sending rpc request to Presnt.NextSlide: ", result)
+  local result = vim.fn.rpcrequest(chan, "Present.NextSlide")
+  vim.fn.chanclose(chan)
+  print("result from sending rpc request to Presnt.NextSlide: ", vim.inspect(result))
 end
 
 return M
--- main.go
+++ main.go
@@ -2,33 +2,56 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"net/rpc"
+	"reflect"
+
+	"github.com/hashicorp/go-msgpack/codec"
 )
 
 type Present struct {
 }
 
-func (p *Present) NextSlide(reply *string) error {
+func (p *Present) NextSlide(args any, reply *string) error {
 	*reply = "going to next slide"
+	fmt.Printf("doing cmd: %v\n", *reply)
 	return nil
 }
 
 func main() {
-	presentor := new(Present)
-	rpc.Register(presentor)
+	go func() {
+		http.Handle("/", http.FileServer(http.Dir("./")))
+		fmt.Println("serving files from ./ on :8080")
+		err := http.ListenAndServe(":8080", nil) 
+		if err != nil {
+			fmt.Println("error starting server")
+		}
+	}()
 
-	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("./"))
+	go func() {
+		presentor := new(Present)
+		rpc.Register(presentor)
 
-	mux.Handle("/", fs)
-	mux.Handle(rpc.DefaultRPCPath, rpc.DefaultServer)
-	mux.Handle(rpc.DefaultDebugPath, rpc.DefaultServer)
+		listener, err := net.Listen("tcp", ":8081")
+		if err != nil {
+			fmt.Println("PRC listening error: ", err)
+		}
+		fmt.Println("MessagePack RPC server running on :8081")
+		
+		var mh codec.MsgpackHandle
+		mh.MapType = reflect.TypeOf(map[string]any(nil))
+	
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("RPC error accepting connection: ", err)
+				continue
+			}
+			go rpc.ServeCodec(codec.MsgpackSpecRpc.ServerCodec(conn, &mh))
+		}
 
-	fmt.Println("serving files from ./ on :8080")
+	}()
 
-	err := http.ListenAndServe(":8080", mux) 
-	if err != nil {
-		fmt.Println("error starting server")
-	}
+	select {}
 }
