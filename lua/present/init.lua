2029721b81abf8e94b31c0e206d295c86749b367
e-mar404 <egmarinn@gmail.com>
2025-08-20 19:25:22 -0500

    rpc call and dont auto start

--- lua/present/init.lua
+++ lua/present/init.lua
@@ -1,15 +1,10 @@
-local config = require "present.config"
+local api = require "present.api"
 
 local M = {}
 
 M.present = function ()
-  local addr = config.rpc.host .. ":" .. config.rpc.port
-  local chan = vim.fn.sockconnect("tcp", addr, {
-    rpc = true,
-  })
+  local result = api.rpc_call("Present.SetPath", {})
 
-  local result = vim.fn.rpcrequest(chan, "Present.SetPath", vim.fn.expand("%:p:h"))
-  vim.fn.chanclose(chan)
   print("result from sending rpc request to Present.SetPath: ", vim.inspect(result))
 end
 
